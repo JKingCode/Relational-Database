1ef5488d8e0dcf920815737d1cc61bb7b15a8973
Joshua King <josh.king793@gmail.com>
2020-06-10 18:14:32 -0600

    Add files via upload

---
+++ Scanner/Parameter.h
@@ -0,0 +1,31 @@
+//#ifndef PARAMETER_H
+//#define PARAMETER_H
+#pragma once
+
+
+//#include "parser.h"
+//#include "expression.h"
+#include "Token.h"
+#include <vector>
+//#include <stdexcept>
+using namespace std;
+
+class parameter {
+protected:
+	vector<string> parameters;//not using
+	string para;
+	Token param;
+	string paramEx;
+	bool isTok = false;
+	bool isEx = false;
+public:
+	parameter();
+	parameter(Token paramTok);
+	parameter(string express);
+	string toString();
+
+};
+
+
+
+//#endif
